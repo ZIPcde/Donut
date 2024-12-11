8f48d389b727c68ab29c6b573280ec78cec1b993
ZIPcde <votmoyyashik100@gmail.com>
2024-12-11 22:56:39 +0300

    local path added

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 // src/config.js
 const config = {
-    baseURL: process.env.VUE_APP_API_BASE_URL || 'http://51.250.112.195:3000',
-    imageBaseURL: process.env.VUE_APP_IMAGE_BASE_URL || 'http://51.250.112.195:3000/api/images',
-  };
-  
-  export default config;
-  
+    baseURL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:3000',
+    imageBaseURL: process.env.VUE_APP_IMAGE_BASE_URL || 'http://localhost:3000/api/images',
+};
+
+export default config;

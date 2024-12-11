dbb186fa5e4155aa868c7eba63e0567dbb196275
ZIPcde <votmoyyashik100@gmail.com>
2024-12-11 22:19:31 +0300

    AboutPage.vue & ContactsPage.vue added

--- src/views/ContactsPage.vue
+++ src/views/ContactsPage.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-      <h1>Контакты</h1>
+    <div class="h1_holder">
+      <h1 class="h1">Donut bar</h1>
+      <h2 class="h2">Контакты</h2>
+      <h3 class="h3">Пишите, звоните, стучите!</h3>
+      <h3 class="h3">+7-999-99-99-97</h3>
+      <h3 class="h3">+7-999-99-99-98</h3>
+      <h3 class="h3">+7-999-99-99-99</h3>
+      <h3 class="h3">DonutBar@sobaka.ru</h3>
+      <h3 class="h3">VKontakte</h3>
     </div>
   </template>
   

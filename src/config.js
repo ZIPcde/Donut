73820612b95d00dd6c6caa67a753d4f383fb865b
ZIPcde <votmoyyashik100@gmail.com>
2024-12-14 09:32:26 +0300

    relative path in config.js added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const config = {
     baseURL: process.env.VUE_APP_API_BASE_URL || 'http://api:3000',
-    imageBaseURL: process.env.VUE_APP_IMAGE_BASE_URL || 'http://api:3000/api/images',
+    imageBaseURL: process.env.VUE_APP_IMAGE_BASE_URL || '/images',
 };
 
 export default config;
